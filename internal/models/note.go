30cb4bed67651f901cbea12e0a8ad99e4ead993e
mizhgun <m.a.ugryumov@outlook.com>
2024-02-22 23:09:50 +0300

    note model changed

--- internal/models/note.go
+++ internal/models/note.go
@@ -1,14 +1,14 @@
 package models
 
 import (
+	"encoding/json"
 	"time"
 )
 
 type Note struct {
-	Id         int       `json:"id"`
-	Title      string    `json:"title"`
-	Content    string    `json:"content,omitempty"`
-	CreateTime time.Time `json:"create_time"`
-	UpdateTime time.Time `json:"update_time"`
-	OwnerId    int       `json:"owner_id"`
+	Id         int             `json:"id"`
+	Data       json.RawMessage `json:"data"`
+	CreateTime time.Time       `json:"create_time"`
+	UpdateTime time.Time       `json:"update_time,omitempty"`
+	OwnerId    int             `json:"owner_id"`
 }
