1e7382adc82dfcb506a5eaf0e1450d197a6b27f6
mizhgun <m.a.ugryumov@outlook.com>
2024-02-27 20:48:36 +0300

    improved validation & postman collection

--- internal/models/auth.go
+++ internal/models/auth.go
@@ -1,15 +1,24 @@
 package models
 
 import (
+	"errors"
 	"fmt"
+	"regexp"
+
 	"github.com/satori/uuid"
 )
 
 const (
-	minUsernameLength = 2
+	minUsernameLength = 4
 	maxUsernameLength = 12
-	minPasswordLength = 8
-	maxPasswordLength = 64
+	minPasswordLength = 9
+	maxPasswordLength = 19
+)
+
+var (
+	usernameRegexp         = regexp.MustCompile("^[0-9A-Za-z_-]+$")
+	passwordRegexp         = regexp.MustCompile("^[0-9A-Za-z#$%&_-]+$")
+	notLessOneLetterRegexp = regexp.MustCompile("[A-Za-z]")
 )
 
 type UserFormData struct {
@@ -24,9 +33,18 @@ func (form *UserFormData) Validate() error {
 	if len(runedUsername) < minUsernameLength || len(runedUsername) > maxUsernameLength {
 		return fmt.Errorf("username length must be from %d to %d characters", minUsernameLength, maxUsernameLength)
 	}
+	if !usernameRegexp.MatchString(form.Username) {
+		return errors.New("username can only include symbols: A-Z, a-z, 0-9, _, - ")
+	}
 
 	if len(runedPassword) < minPasswordLength || len(runedUsername) > maxPasswordLength {
 		return fmt.Errorf("password length must be from %d to %d characters", minPasswordLength, maxPasswordLength)
+	}
+	if !passwordRegexp.MatchString(form.Password) {
+		return errors.New("password can only include symbols: A-Z, a-z, 0-9, #, $, %, &, _, - ")
+	}
+	if !notLessOneLetterRegexp.MatchString(form.Password) {
+		return errors.New("password must include at least 1 letter (A-Z, a-z)")
 	}
 
 	return nil
