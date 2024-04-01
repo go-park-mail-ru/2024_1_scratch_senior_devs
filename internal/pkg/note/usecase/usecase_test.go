928010417493eee553e6c03b5b4737ffa6d2babf
Alladan04 <alla.danielyan.2004@mail.ru>
2024-04-01 14:23:46 +0300

    some note usecase tests done

--- internal/pkg/note/usecase/usecase_test.go
+++ internal/pkg/note/usecase/usecase_test.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"log/slog"
 	"os"
+	"reflect"
 	"testing"
 	"time"
 
@@ -119,6 +120,141 @@ func TestNoteUsecase_GetAllNotes(t *testing.T) {
 			if !assert.Equal(t, got, tt.want) {
 				t.Errorf("NoteUsecase.GetAllNotes() = %v, want %v", got, tt.want)
 			}
+		})
+	}
+}
+
+func TestNoteUsecase_GetNote(t *testing.T) {
+
+	type args struct {
+		ctx    context.Context
+		noteId uuid.UUID
+		userId uuid.UUID
+	}
+	tests := []struct {
+		name       string
+		repoMocker func(context context.Context, repo *mock_note.MockNoteRepo, nId uuid.UUID)
+		args       args
+		want       models.Note
+		wantErr    bool
+	}{
+		{
+			name: "TestSuccess",
+			repoMocker: func(ctx context.Context, repo *mock_note.MockNoteRepo, nId uuid.UUID) {
+				mockResp := models.Note{ //мок ответа от уровня репозитория
+					Id:         uuid.FromStringOrNil("c80e3ea8-0813-4731-b6ee-b41604c56f95"),
+					OwnerId:    uuid.FromStringOrNil("a233ea8-0813-4731-b12e-b41604c56f95"),
+					UpdateTime: time.Time{},
+					CreateTime: time.Time{},
+					Data:       nil,
+				}
+
+				repo.EXPECT().ReadNote(ctx, nId).Return(mockResp, nil).Times(1)
+			},
+			args: args{
+				context.Background(),
+				uuid.FromStringOrNil("c80e3ea8-0813-4731-b6ee-b41604c56f95"),
+				uuid.FromStringOrNil("a233ea8-0813-4731-b12e-b41604c56f95"),
+			},
+			want: models.Note{
+				Id:         uuid.FromStringOrNil("c80e3ea8-0813-4731-b6ee-b41604c56f95"),
+				OwnerId:    uuid.FromStringOrNil("a233ea8-0813-4731-b12e-b41604c56f95"),
+				UpdateTime: time.Time{},
+				CreateTime: time.Time{},
+				Data:       nil,
+			},
+
+			wantErr: false,
+		},
+		{
+			name: "TestFail",
+			repoMocker: func(ctx context.Context, repo *mock_note.MockNoteRepo, nId uuid.UUID) {
+				mockResp := models.Note{ //мок ответа от уровня репозитория
+
+				}
+
+				repo.EXPECT().ReadNote(ctx, nId).Return(mockResp, errors.New("error")).Times(1)
+			},
+			args: args{
+				context.Background(),
+				uuid.FromStringOrNil("c80e3ea8-0813-4731-b6ee-b41604c56f95"),
+				uuid.FromStringOrNil("a233ea8-0813-4731-b12e-b41604c56f95"),
+			},
+			want: models.Note{},
+
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctl := gomock.NewController(t)
+			defer ctl.Finish()
+			repo := mock_note.NewMockNoteRepo(ctl)
+			uc := CreateNoteUsecase(repo, testLogger)
+
+			tt.repoMocker(context.Background(), repo, tt.args.noteId)
+
+			got, err := uc.GetNote(tt.args.ctx, tt.args.noteId, tt.args.userId)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NoteUsecase.GetNote() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NoteUsecase.GetNote() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestNoteUsecase_CreateNote(t *testing.T) {
+
+	type args struct {
+		ctx      context.Context
+		userId   uuid.UUID
+		noteData []byte
+	}
+	tests := []struct {
+		name       string
+		repoMocker func(context context.Context, repo *mock_note.MockNoteRepo)
+		args       args
+		wantErr    bool
+		want       models.Note
+	}{
+		{
+			name: "TestSuccess",
+			repoMocker: func(ctx context.Context, repo *mock_note.MockNoteRepo) {
+
+				repo.EXPECT().CreateNote(ctx, gomock.Any()).Return(nil).Times(1)
+
+			},
+			args: args{
+				ctx:      context.Background(),
+				userId:   uuid.FromStringOrNil("a233ea8-0813-4731-b12e-b41604c56f95"),
+				noteData: []byte("{\"title\":\"title\"}"),
+			},
+			wantErr: false,
+			want:    models.Note{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			ctl := gomock.NewController(t)
+			defer ctl.Finish()
+			repo := mock_note.NewMockNoteRepo(ctl)
+			uc := CreateNoteUsecase(repo, testLogger)
+
+			tt.repoMocker(context.Background(), repo)
+
+			got, err := uc.CreateNote(tt.args.ctx, tt.args.userId, tt.args.noteData)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("NoteUsecase.CreateNote() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if reflect.TypeOf(got) != reflect.TypeOf(tt.want) {
+				t.Errorf("NoteUsecase.CreateNote() returned %v, want %v", err, tt.wantErr)
+				return
+			}
+
 		})
 	}
 }
