a878c428200af44ccf5a5a1895ecafe87d3a833c
Alladan04 <alla.danielyan.2004@mail.ru>
2024-04-27 12:03:57 +0300

    v,kvnsvlk

--- internal/pkg/survey/delivery/http/handlers.go
+++ internal/pkg/survey/delivery/http/handlers.go
@@ -3,7 +3,7 @@ package http
 import "github.com/go-park-mail-ru/2024_1_scratch_senior_devs/internal/pkg/note/delivery/grpc/gen"
 
 type SurveyHandler struct {
-	client gen.NoteClient
+	client gen.StatClient
 }
 
 func CreateSurveyHandler(client gen.NoteClient) *SurveyHandler {
