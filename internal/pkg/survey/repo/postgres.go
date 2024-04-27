96ff0ce1573cd9ec9b05a37d898e2335d0613572
mizhgun <m.a.ugryumov@outlook.com>
2024-04-27 11:31:45 +0300

    models

--- internal/pkg/survey/repo/postgres.go
+++ internal/pkg/survey/repo/postgres.go
@@ -1,1 +1,13 @@
 package repo
+
+import "github.com/jackc/pgtype/pgxtype"
+
+type SurveyRepo struct {
+	db pgxtype.Querier
+}
+
+func CreateSurveyRepo(db pgxtype.Querier) *SurveyRepo {
+	return &SurveyRepo{
+		db: db,
+	}
+}
--- internal/pkg/survey/usecase/usecase.go
+++ internal/pkg/survey/usecase/usecase.go
@@ -1,1 +1,15 @@
 package usecase
+
+import (
+	"github.com/go-park-mail-ru/2024_1_scratch_senior_devs/internal/pkg/survey"
+)
+
+type SurveyUsecase struct {
+	repo survey.SurveyRepo
+}
+
+func CreateSurveyUsecase(repo survey.SurveyRepo) *SurveyUsecase {
+	return &SurveyUsecase{
+		repo: repo,
+	}
+}
