ca47ec8b03ff6618021be0fad31026ac8942d24b
mizhgun <m.a.ugryumov@outlook.com>
2024-04-27 12:04:35 +0300

    grpc

--- internal/pkg/survey/delivery/grpc/handlers.go
+++ internal/pkg/survey/delivery/grpc/handlers.go
@@ -1,1 +1,15 @@
 package grpc
+
+import (
+	"github.com/go-park-mail-ru/2024_1_scratch_senior_devs/internal/pkg/survey"
+	generatedSurvey "github.com/go-park-mail-ru/2024_1_scratch_senior_devs/internal/pkg/survey/delivery/grpc/gen"
+)
+
+type GrpcSurveyHandler struct {
+	generatedSurvey.StatServer
+	uc survey.SurveyUsecase
+}
+
+func NewGrpcSurveyHandler(uc survey.SurveyUsecase) *GrpcSurveyHandler {
+	return &GrpcSurveyHandler{uc: uc}
+}
