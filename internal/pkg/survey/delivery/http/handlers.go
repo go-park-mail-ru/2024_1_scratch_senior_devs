4c17b0393de29c6850ffe6cfe01f42cdba660bdf
Alladan04 <alla.danielyan.2004@mail.ru>
2024-04-27 12:16:39 +0300

    scd

--- internal/pkg/survey/delivery/http/handlers.go
+++ internal/pkg/survey/delivery/http/handlers.go
@@ -1,7 +1,15 @@
 package http
 
 import (
+	"encoding/json"
+	"errors"
+	"log/slog"
+	"net/http"
+
+	"github.com/go-park-mail-ru/2024_1_scratch_senior_devs/internal/models"
 	"github.com/go-park-mail-ru/2024_1_scratch_senior_devs/internal/pkg/survey/delivery/grpc/gen"
+	"github.com/go-park-mail-ru/2024_1_scratch_senior_devs/internal/pkg/utils/log"
+	"github.com/go-park-mail-ru/2024_1_scratch_senior_devs/internal/pkg/utils/responses"
 )
 
 type SurveyHandler struct {
@@ -12,4 +20,50 @@ func CreateSurveyHandler(client gen.StatClient) *SurveyHandler {
 	return &SurveyHandler{
 		client: client,
 	}
+}
+
+func (h *SurveyHandler) AddNote(w http.ResponseWriter, r *http.Request) {
+	logger := log.GetLoggerFromContext(r.Context()).With(slog.String("func", log.GFN()))
+
+	// jwtPayload, ok := r.Context().Value(config.PayloadContextKey).(models.JwtPayload)
+	// if !ok {
+	// 	log.LogHandlerError(logger, http.StatusUnauthorized, responses.JwtPayloadParseError)
+	// 	w.WriteHeader(http.StatusUnauthorized)
+	// 	return
+	// }
+
+	payload := models.Vote{}
+	if err := responses.GetRequestData(r, &payload); err != nil {
+		log.LogHandlerError(logger, http.StatusBadRequest, responses.ParseBodyError+err.Error())
+		responses.WriteErrorMessage(w, http.StatusBadRequest, errors.New("incorrect data format"))
+		return
+	}
+
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.LogHandlerError(logger, http.StatusBadRequest, responses.ParseBodyError+err.Error())
+		responses.WriteErrorMessage(w, http.StatusBadRequest, errors.New("incorrect data format"))
+		return
+	}
+
+	err = h.client.
+	if err != nil {
+		log.LogHandlerError(logger, http.StatusBadRequest, err.Error())
+		responses.WriteErrorMessage(w, http.StatusBadRequest, errors.New("invalid query"))
+		return
+	}
+
+	resultNote, err := getNote(protoNote.Note)
+	if err != nil {
+		log.LogHandlerError(logger, http.StatusInternalServerError, err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	if err := responses.WriteResponseData(w, resultNote, http.StatusCreated); err != nil {
+		log.LogHandlerError(logger, http.StatusInternalServerError, responses.WriteBodyError+err.Error())
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	log.LogHandlerInfo(logger, http.StatusOK, "success")
 }
