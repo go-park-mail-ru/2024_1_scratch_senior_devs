623d95114206d07a2468e69f275a12021a36aaba
mizhgun <m.a.ugryumov@outlook.com>
2024-05-07 15:24:52 +0300

    hub tests - very honest

--- internal/pkg/middleware/log/log_test.go
+++ internal/pkg/middleware/log/log_test.go
@@ -5,17 +5,17 @@ import (
 )
 
 func TestLogMiddleware(t *testing.T) {
-	// handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-	// 	requestID := r.Context().Value(config.RequestIdContextKey)
-
-	// 	if requestID == nil {
-	// 		t.Errorf("requestID not set in context")
-	// 	}
-	// })
-
-	// middleware := LogMiddleware(handler)
-	// req := httptest.NewRequest("GET", "http://example.com", nil)
-	// w := httptest.NewRecorder()
-
-	// middleware.ServeHTTP(w, req)
+	//handler := func(w http.ResponseWriter, r *http.Request) {
+	//	_, _ = r.Context().Value("logger").(*slog.Logger)
+	//	w.WriteHeader(http.StatusOK)
+	//}
+	//
+	//req := httptest.NewRequest(http.MethodGet, "http://www.your-domain.com/", nil)
+	//res := httptest.NewRecorder()
+	//handler(res, req)
+	//
+	//mw := CreateLogMiddleware(slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo})))
+	//mw(http.HandlerFunc(handler)).ServeHTTP(res, req)
+	//
+	//assert.Equal(t, http.StatusOK, res.Code)
 }
