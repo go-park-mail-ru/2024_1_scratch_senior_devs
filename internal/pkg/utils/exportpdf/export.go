88a7cbfb236ce4bd02a7a0224e4bad81ca5db46e
mizhgun <m.a.ugryumov@outlook.com>
2024-05-12 16:39:21 +0300

    export to pdf: first generation

--- internal/pkg/utils/exportpdf/export.go
+++ internal/pkg/utils/exportpdf/export.go
@@ -1,6 +1,7 @@
 package exportpdf
 
 import (
+	"errors"
 	"os"
 	"strconv"
 	"time"
@@ -18,29 +19,29 @@ func GeneratePDF(basicHTML string) ([]byte, string, error) {
 
 	if _, err := os.Stat("cloneHTML/"); os.IsNotExist(err) {
 		if err := os.Mkdir("cloneHTML/", 0777); err != nil {
-			return nil, "", err
+			return nil, "", errors.New("1: " + err.Error())
 		}
 	}
 
 	dir, err := os.Getwd()
 	if err != nil {
-		return nil, "", err
+		return nil, "", errors.New("2: " + err.Error())
 	}
 	defer os.RemoveAll(dir + "/cloneHTML")
 
 	if err := os.WriteFile("cloneHTML/"+strconv.FormatInt(t, 10)+".html", []byte(noteHTML), 0644); err != nil {
-		return nil, "", err
+		return nil, "", errors.New("3: " + err.Error())
 	}
 
 	file, err := os.Open("cloneHTML/" + strconv.FormatInt(t, 10) + ".html")
 	if err != nil {
-		return nil, "", err
+		return nil, "", errors.New("4: " + err.Error())
 	}
 	defer file.Close()
 
 	generator, err := wkhtmltopdf.NewPDFGenerator()
 	if err != nil {
-		return nil, "", err
+		return nil, "", errors.New("5: " + err.Error())
 	}
 
 	generator.NoPdfCompression.Set(true)
@@ -49,7 +50,7 @@ func GeneratePDF(basicHTML string) ([]byte, string, error) {
 	generator.Dpi.Set(300)
 
 	if err = generator.Create(); err != nil {
-		return nil, "", err
+		return nil, "", errors.New("6: " + err.Error())
 	}
 
 	return generator.Bytes(), getNoteTitle(basicHTML), nil
